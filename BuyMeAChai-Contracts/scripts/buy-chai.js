399d4cb5a5d510b4ee7c1a103ec647007aab05a8
gokulnair2001 <gokulnair.2001@gmail.com>
2022-05-09 11:13:17 +0530

    Scrip fix

--- BuyMeAChai-Contracts/scripts/buy-chai.js
+++ BuyMeAChai-Contracts/scripts/buy-chai.js
@@ -5,6 +5,31 @@
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
+// Returns the Ether balance of a given address
+async function getBalance(address) {
+  const balanceBigInt = await hre.waffle.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+// Logs the Ether balances for a list of addresses.
+async function printBalance(addresses) {
+  let idx = 0;
+  for (const address of addresses) {
+    console.log(`Address ${idx} balance:`, await getBalance(address));
+    idx++;
+  }
+}
+
+async function printMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`);
+  }
+}
+
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
